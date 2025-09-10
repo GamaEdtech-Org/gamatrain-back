ce76b006f6685cdb356fe669335da1953c002e1a
sobhanagh <sobhanaghajani893@gmail.com>
2025-09-10 17:17:03 +0330

    edit: container type

--- src/Domain/Enumeration/ContainerType.cs
+++ src/Domain/Enumeration/ContainerType.cs
@@ -13,6 +13,7 @@ namespace GamaEdtech.Domain.Enumeration
 
         [Display]
         public static readonly ContainerType Post = new(nameof(Post), 2);
+
         [Display]
         public static readonly ContainerType User = new(nameof(User), 3);
 
